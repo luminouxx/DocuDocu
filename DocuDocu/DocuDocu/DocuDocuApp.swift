faa58b70b26548a87c95bf6f65bdc02d666ed756
YeongJin Jeong <yungzin98@naver.com>
2024-08-31 17:28:14 +0900

    :tada: Add Project
    target
    - iOS : 16.0 +

---
+++ DocuDocu/DocuDocu/DocuDocuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocuDocuApp.swift
+//  DocuDocu
+//
+//  Created by 정영진 on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct DocuDocuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
